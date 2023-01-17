d9ed805583bff6f0800cff488eb06c93db481355
Raskatov Igor <aura.star@yandex.ru>
2023-01-17 21:52:11 +0300

    Add favicon, add variables

--- src/pages/NotFound/NotFound.props.ts
+++
@@ -1,4 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface NotFoundProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { NotFound } from '../pages';
 import { MainLayout } from '../layouts/MainLayout';
 
 export const AppRouter: FC = () => {
@@ -8,7 +7,7 @@ export const AppRouter: FC = () => {
 		<Routes>
 			<Route path="/" element={<MainLayout />}>
 				<Route index element={'Test'} />
-				<Route path="/*" element={<NotFound />} />
+				<Route path="/*" element={'t'} />
 				<Route path="/example" element={'Example'} />
 			</Route>
 		</Routes>
