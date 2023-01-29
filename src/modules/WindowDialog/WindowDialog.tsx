10d193d4cfe010e326c71b585ce2e5bc19d28bab
Raskatov Igor <aura.star@yandex.ru>
2023-01-29 19:48:40 +0300

    Add clickedItem state on the top of components

--- src/modules/WindowDialog/WindowDialog.tsx
+++ src/modules/WindowDialog/WindowDialog.tsx
@@ -3,10 +3,11 @@ import { WindowDialogProps } from './WindowDialog.props';
 import cn from 'classnames';
 import { FC, useEffect, useRef, useState } from 'react';
 import { Interactions, Message } from '../../components';
-import { TopLine } from '../../ui';
+import { Setting, TopLine, Image } from '../../ui';
 import ScrollToBottom from 'react-scroll-to-bottom';
 import { useAppSelector } from '../../hooks/redux';
 import ArrowIcon from './arrow.svg';
+import { ClickedItemWindow } from '../../components/ClickedItemWindow/ClickedItemWindow';
 
 export const WindowDialog: FC<WindowDialogProps> = ({
 	className,
@@ -21,54 +22,78 @@ export const WindowDialog: FC<WindowDialogProps> = ({
 	const [isHintVisible, setIsHintVisible] = useState<boolean>(
 		localStorage.isHintVisible ? false : true
 	);
+	const { clickedItem } = useAppSelector((state) => state.clickedItemReducer);
 
 	useEffect(() => {
 		bottomDiv.current?.addEventListener('resize', () => {
 			bottomDiv.current?.scrollIntoView({ behavior: 'auto' });
 		});
 	}, [bottomDiv]);
+
 	setTimeout(() => {
 		setIsHintVisible(false);
-		localStorage.isHintVisible = 0;
+		localStorage.isHintVisible = 42;
 	}, 5000);
+
 	return (
-		<div
-			className={cn(className, styles.WindowDialog, {
-				[styles.centered]: !isCurrentDiaglosChosen,
-			})}
-			{...props}>
-			{isCurrentDiaglosChosen ? (
-				<>
-					<TopLine />
-					<ScrollToBottom
-						initialScrollBehavior="auto"
-						className={styles.dialogs}>
-						{currentDialog.messages &&
-							currentDialog.messages.length &&
-							currentDialog.messages.map((d) => (
-								<Message
-									key={d.id}
-									className={d.his ? styles.his : styles.your}
-									{...d}>
-									{d.text}
-								</Message>
-							))}
-						<div ref={bottomDiv} />
-					</ScrollToBottom>
-					<Interactions />
-				</>
-			) : (
-				<p className={styles.select}>
-					Выберите, кому хотели бы написать
-					<span
-						className={cn(styles.hint, {
-							[styles.visible]: !isHintVisible,
-						})}>
-						<span>Темную тему можно задать в настройках</span>
-						<ArrowIcon className={styles.arrow} />
-					</span>
-				</p>
+		<>
+			<div
+				className={cn(className, styles.WindowDialog, {
+					[styles.centered]: !isCurrentDiaglosChosen,
+				})}
+				{...props}>
+				{isCurrentDiaglosChosen ? (
+					<>
+						<TopLine />
+						<ScrollToBottom
+							initialScrollBehavior="auto"
+							className={styles.dialogs}>
+							{currentDialog.messages &&
+								currentDialog.messages.length &&
+								currentDialog.messages.map((d) => (
+									<Message
+										key={d.id}
+										className={
+											d.his ? styles.his : styles.your
+										}
+										{...d}>
+										{d.text}
+									</Message>
+								))}
+							<div ref={bottomDiv} />
+						</ScrollToBottom>
+						<Interactions />
+					</>
+				) : (
+					<p className={styles.select}>
+						Выберите, кому хотели бы написать
+						<span
+							className={cn(styles.hint, {
+								[styles.visible]: !isHintVisible,
+							})}>
+							<span>Темную тему можно задать в настройках</span>
+							<ArrowIcon className={styles.arrow} />
+						</span>
+					</p>
+				)}
+			</div>
+
+			{clickedItem && 'filename' in clickedItem && (
+				<ClickedItemWindow
+					Component={Image}
+					alt={clickedItem.filename}
+					src={clickedItem.url}
+				/>
 			)}
-		</div>
+
+			{clickedItem && 'icon' in clickedItem && (
+				<ClickedItemWindow
+					Component={Setting}
+					color={clickedItem.color}
+					name={clickedItem.name}
+					icon={clickedItem.icon}
+				/>
+			)}
+		</>
 	);
 };
